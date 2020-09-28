aa60d0191ab0ddebc553acedc6fedb7b524da873
ericdonnelly <ericdonnelly601@gmail.com>
2020-09-27 23:07:56 -0500

    added barchart

--- docs/static/js/app.js
+++ docs/static/js/app.js
@@ -13,31 +13,59 @@ function init() {
           .property("value", id);
         });
       
+      // var random_number = Math.floor(Math.random() * 10);
       // first id
-      let first_id = subjectIds[0];
+      let first_id = subject_ids[0];
   
       // call functions
       update_charts(first_id);
-      update_metadata(first_id);
+      // update_metadata(first_id);
     });
   }
   
-  // // function to update charts
-  // function update_charts() {
-  //   // select data with d3
-  //   d3.json("data/samples.json").then((data) => {
-  //     // data selection will go here
-  //   }
-  // }
+  // function to update charts
+  function update_charts(sample) {
+    // select data with d3
+    d3.json("data/samples.json").then((d) => {
+      // data selection
+      var samples = d.samples;
+      var results = samples.filter(sampleobject => sampleobject.id == sample);
+      var result = results[0];
+      var values = result.sample_values;
+      var otu_ids = result.otu_ids;
+      var otu_labels = result.otu_labels;   
+      // build bar chart
+      var trace1 = {
+          x: values.slice(0,10).reverse(),
+          y: otu_ids.slice(0,10).map(otuID => `OTU ${otuID}`).reverse(),
+          text: otu_labels.slice(0,10).reverse(),
+          name: "Bar Chart",
+          type: "bar",
+          orientation: "h"
+      };
+      var data = [trace1];
+      var layout = {
+          title: "Top Ten OTUs for Individual " +sample,
+          margin: {l: 100, r: 100, t: 100, b: 100}
+      };
+      Plotly.newPlot("bar", data, layout);  
+      });
+    }
   
-  // // function to update metadata
+  // function to update metadata
   // function update_metadata() {
-  //   // select data with d3
-  //   d3.json("data/samples.json").then((data) => {
-  //   // data selection will go here
-  //   }
+  //   // // // select data with d3
+  //   // d3.json("data/samples.json").then((data) => {
+  //   // // data selection will go here
+  //   // }
   // }
 
+
+  function optionChanged(select_new) {
+    // update based on selection
+    update_charts(select_new);
+    // update_metadata(select_new);
+  }
 
   // call initialize dashboard function
   init();
